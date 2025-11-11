f442e266fc2711963d7d127994a25c1eb9ca8c0b
Eduardo Hoyos <edu.hoyos92@gmail.com>
2025-11-11 10:29:33 -0500

    Initial Commit

---
+++ URLCachevsNSCache/URLCachevsNSCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLCachevsNSCacheApp.swift
+//  URLCachevsNSCache
+//
+//  Created by Eduardo Hoyos on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct URLCachevsNSCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
